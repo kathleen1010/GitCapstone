f750ba6896a2fb984e8a9bddd5feef400ce7d828
kathleen1010 <154378042+kathleen1010@users.noreply.github.com>
2024-02-14 00:13:41 -0500

    added Counter, updated getgrams to add subgrams
    completed work on getgrams function.
    added removejunk function to get out grams which start or end with
    punctuation.
    added counter function

--- Capstone_wk5.py
+++ Capstone_wk5.py
@@ -8,25 +8,7 @@ Created on Wed Jan 17 10:32:35 2024
 #%%reset 
 #%reset
 
-#%% test space 
-
-#generator test
-
-sample = ['edward', 'bella', 'jacob', 'renesmee', 'charlie']
-
-genx = (name for name in sample)
-geny = (len(name) for name in sample) 
-
-namelen = dict(zip(genx, geny))
-namelen['edward']
-
-namelen.keys()
-
-twice = {key:val*2 for (key,val) in namelen.items()}
-twice
-
-twice = {key:val*2 for (key,val) in namelen.items() if 'c' not in key}
-twice
+#%% test
 #%% Import moses
 filepath = 'C:/Users/Kathleen/Documents/GitCapstone/en-zh.txt/TED2013.en-zh_en.txt'
 
@@ -40,90 +22,153 @@ with open(filepath2, encoding = "utf-8") as f2:
 
 moses_dict = dict(zip(moses_zh, moses_en))
 
-#%% check dictionary. 
-
-import difflib
-
-#look up an english entry based on a chinese entry we know exists. 
-moses_dict['海洋是一个非常复杂的事物。']
-
+#check dictionary. 
 moses_dict['另一种情况']
-
-
-#a quote we _sort of_ know exists. 
-#this comes from XML  -- get need to get a fuzzy match to find out how it is spaced/ punctuated in the moses file. 
-test1 = "而另一种 “方位细胞” ， 我之前没有提到它 ， 它们像指南针一样 ， 都是根据你的朝向来作出反应的"
-difflib.get_close_matches(test1,moses_dict.keys())
-
-#this key finds the correct translation. 
-moses_dict['而另一种“方位细胞”， 我之前没有提到它， 它们像指南针一样，都是根据你的朝向来作出反应的。'] 
-
-
-test2 = '他们是每侧最边上的两位 。 这是棒球诊所的一部分 ， 在这里我们尝试 棒球联赛 ， 国务院 ， （ 它建起了外交基础 ） 在军队中的棒球运动员 ， 也就是那些能真正上战场的人 ， 之间的合作 ， 并且他们把这种诊所 开到了拉丁美洲和加勒比地区 ， 开到了洪都拉斯 ， 开到了尼加拉瓜 ， 开到了所有中美洲和加勒比的 棒球很流行国家 ， 而且它创造了安全感 '
-difflib.get_close_matches(test2,moses_dict.keys())
-#once we have the right spacing, we plug that back in to the dictionary to get the value. 
-test2 = '他们是每侧最边上的两位。 这是棒球诊所的一部分， 在这里我们尝试 棒球联赛， 国务院， （它建起了外交基础） 在军队中的棒球运动员， 也就是那些能真正上战场的人，之间的合作， 并且他们把这种诊所 开到了拉丁美洲和加勒比地区， 开到了洪都拉斯，开到了尼加拉瓜， 开到了所有中美洲和加勒比的 棒球很流行国家， 而且它创造了安全感。'
-moses_dict[test2]
-
 
 #%%remove multi-sentence 
 
 sentence_dict = {key:value for (key, value) in moses_dict.items() if '。' not in key}
-
-#%% search 
+#len(sentence_dict)
+#%% basic search functions
 
 def searchterm(term): 
     examples = list()
     for sent in sentence_dict.keys(): 
         if term in sent: 
-            examples.append(sent)
+            examples.append(sent.replace(' ',''))
     return examples
 
-nose = searchterm('鼻子')
-
+# nose = searchterm('鼻子')
+# nose
+# searchterm('恢复听力')
 
 def searchterm_dict(term): 
     examples = {key:value for (key, value) in sentence_dict.items() if term in key}
     return examples
 
-nose_dict = searchterm_dict('鼻子')
-nose_dict
+# nose_dict = searchterm_dict('鼻子')
+# nose_dict
 
-#%% input search 
-def user_searchterm(): 
-    target_term = input('Please enter a search term of no more than two characters:')
-    if len(target_term) >2: 
-        print('Invalid search term.') 
-    else: 
-        results = searchterm(target_term)
-    return results
-user_searchterm() 
+#%% user input search - for chinese
+# def user_searchterm(): 
+#     target_term = input('Please enter a search term of no more than two characters:')
+#     if len(target_term) >2: 
+#         print('Invalid search term.') 
+#     else: 
+#         results = searchterm(target_term)
+#     return results
+# user_searchterm() 
 
-#%% endless search 
-donesy = ['DONE', 'Done', 'done']
-def user_search_loop():
-    while True: 
-        target_term = input('\n Please enter a search term of no more than 4 characters, or enter "done" to end session:')
-        if target_term in donesy: 
-            print('Thank you. 谢谢。')
-            break
-        elif len(target_term) >4: 
-            print('\n Invalid search term.') 
-        else: 
-            results = searchterm_dict(target_term)
-            print(results)
-            print('\n' +str(len(results))+' total results')
+ #%% endless search - for pairs
+# donesy = ['DONE', 'Done', 'done']
+# def user_search_loop():
+#     while True: 
+#         target_term = input('\n Please enter a search term of no more than 4 characters, or enter "done" to end session:')
+#         if target_term in donesy: 
+#             print('Thank you. 谢谢。')
+#             break
+#         elif len(target_term) >4: 
+#             print('\n Invalid search term.') 
+#         else: 
+#             results = searchterm_dict(target_term)
+#             print(results)
+#             print('\n' +str(len(results))+' total results')
 
-user_search_loop() 
+# user_search_loop() 
 
-#%% 
+#%% Count Frequency
 import re
 
-#it's just going to be 
-# "match char + plus target" something like... 
-# \r'(\w{target_term}')
-#\r'(\w\w{target_term}') and then reverse order
-#figure out how to access the captured phrase. 
-def getgrams(): 
-    ()
+def pre_subgrams(gram, target, phrasebook):
+    phrasebook.append(gram)
+    for i in range(4,1,-1): 
+        if len(gram) - len(target) >= i: 
+            #DROP THE LAST CHARACTER AND REPEAT
+            gram = gram[1:]
+            phrasebook.append(gram)
+    return phrasebook
+
+def post_subgrams(gram, target, phrasebook):
+    phrasebook.append(gram)
+    for i in range(4,1,-1): 
+        if len(gram) - len(target) >= i: 
+            #DROP THE LAST CHARACTER AND REPEAT
+            gram = gram[:-1]
+            phrasebook.append(gram)
+    return phrasebook  
+
+def removejunk(phrasebook):
+    """Remove the entire entry if it starts or ends with a junk character, aka space or punctuation. 
+    We have already kept the shortened forms of all of these grams, so keeping them or shortening them now would be duplicative. 
+    """
+    junk = ['(', ')', ',', '.' , '。', ' ', '!', '！', '%', '，']
+    for i in range(4): 
+        for phrase in phrasebook:
+            if phrase[0] in junk:
+                phrasebook.remove(phrase)
+            elif phrase[-1] in junk: 
+                phrasebook.remove(phrase)
+            else:
+                continue
+    #removejunk(phrasebook)
+    return phrasebook
+
+# sampy = ['看起来', ' 好的儿', '! 你是我的好朋友', '女儿在哪儿', ',女儿在哪儿', '你看 ', '你看']
+# result = removejunk(sampy)
+# result
+
+def getgrams(target): 
+    sentences = searchterm(target)
+    #print(sentences)
+    phrasebook = list()
+    regex_pre =re.compile(fr'(.{{1,4}}{target})')
+    regex_post=re.compile(fr'({target}.{{1,4}})')
+    for sent in sentences: 
+        #search for phrases
+        result_pre =(re.search(regex_pre, sent))
+        result_post = (re.search(regex_post, sent))
+        #append phrases
+        if result_pre != None:
+            pre_fix = result_pre.groups()[0]
+            phrasebook = pre_subgrams(pre_fix, target, phrasebook)
+        if result_post != None: 
+           post_fix = result_post.groups()[0]
+           phrasebook = post_subgrams(post_fix, target, phrasebook)
+    #remove junk 
+    phrasebook = removejunk(phrasebook)
+        
+    return phrasebook    
+
+listening = getgrams('听力')
+len(listening)
+listening2 = set(listening)
+len(listening2)
+
+#128, 96
+
+#%% Counter Test
+
+from collections import Counter
+
+def countgrams(target): 
+    phrasebook = getgrams(target)
+    counts = Counter(phrasebook)
+    return counts.most_common(20)
+
+
+countgrams('美丽')
+countgrams('正常')
+
+facts = getgrams('美国') 
+print(len(facts))
+
+facts2 = Counter(facts)
+facts2.most_common(20) 
+len(facts2)
+
+#%% test 
+
+
+
+
 
